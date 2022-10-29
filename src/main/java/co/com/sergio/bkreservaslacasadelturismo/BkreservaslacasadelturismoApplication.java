da934bb42cf48ce374063760f086d07094ae9bad
SERGIO RODRÍGUEZ <SARV9208@hotmail.com>
2022-10-29 14:04:22 -0500

    Creación del Back-end

---
+++ src/main/java/co/com/sergio/bkreservaslacasadelturismo/BkreservaslacasadelturismoApplication.java
@@ -0,0 +1,13 @@
+package co.com.sergio.bkreservaslacasadelturismo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BkreservaslacasadelturismoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BkreservaslacasadelturismoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/sergio/bkreservaslacasadelturismo/BkreservaslacasadelturismoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sergio.bkreservaslacasadelturismo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BkreservaslacasadelturismoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
